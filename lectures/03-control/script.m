eefee20d314118d336be8b80d9070fa73978d10b
Xavier Sánchez <xavier.sanchezdz@gmail.com>
2019-09-03 16:35:16 -0500

    Updated with some new scripts for lecture 03

--- lectures/03-control/script.m
+++ lectures/03-control/script.m
@@ -4,6 +4,6 @@ a = 3;
 b = 4;
 c = sqrt(a^2 + b^2);
 
-message = 'If your triangle has sides of %d and %d units, then the hypotenuse is %d units long';
+message = 'If your triangle has sides of %d and %d units, then the hypotenuse is %f units long';
 printthis = sprintf(message, a, b, c);
 disp(printthis)
---
+++ misc/basicsum.m
@@ -0,0 +1,7 @@
+% basic sum
+caja = 1;
+for j = 1:6
+    caja = caja * j;
+end
+
+disp(caja)
